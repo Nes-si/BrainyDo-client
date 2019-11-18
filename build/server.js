f48e03b0a6a2d375b192c3293f1d59bbc0e25f45
Nes-si <golovin.nik@gmail.com>
2019-11-18 20:34:50 +0300

    Сборка. Использование webpack dev server.

--- build/server.js
+++ build/server.js
@@ -1,32 +1,25 @@
-const express = require('express');
 const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const history = require('connect-history-api-fallback');
+const WebpackDevServer = require('webpack-dev-server');
 
 const config = require('./webpack.dev.config');
 
 
-let server = new express();
 let port = process.env.PORT || 11000;
 
-server.use(history());
-
-let compiler = webpack(config);
-server.use(webpackDevMiddleware(compiler, {
-  publicPath: config.output.publicPath,
-  stats: {
-    colors: true,
-    chunks: false
+let server = new WebpackDevServer(
+  webpack(config),
+  {
+    historyApiFallback: true,
+    hot: true,
+    publicPath: config.output.publicPath,
+    stats: {
+      colors: true,
+      chunks: false
+    }
   }
-}));
-server.use(webpackHotMiddleware(compiler));
+);
 
-// serve pure static assets
-server.use('/', express.static('./static'));
-
-
-server.listen(port, error => {
+server.listen(port, 'localhost', error => {
   if (error)
     console.error(error);
   else
